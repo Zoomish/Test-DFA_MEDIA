022f3d250b28ff7f55c2ab8cfccd7cd1f9689fad
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-20 21:36:00 +0300

    "Rename `TOKEN` to `NEXT_PUBLIC_TOKEN` in `.env` and update references in `movie-api.ts` and `envConfig.ts`"

--- src/utils/api/movie-api.ts
+++ src/utils/api/movie-api.ts
@@ -1,13 +1,10 @@
 import { BASE_URL } from "../const";
-import "../envConfig.ts";
-
 export const getMovies = async () => {
-  console.log(process.env.TOKEN);
   return await fetch(`${BASE_URL}/discover/movie`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.TOKEN}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
     },
   }).then(async (res) => res);
 };

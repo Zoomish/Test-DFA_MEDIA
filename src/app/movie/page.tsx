69e69066ae0f410fe442b16931473efd51117914
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-20 21:46:52 +0300

    "Removed JSON stringification of data and added console log of data in Home component"

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -3,13 +3,13 @@ import { useQuery } from "@tanstack/react-query";
 import * as movieAPI from "@/utils/api/movie-api";
 
 export default function Home() {
-
   const { isPending, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () => movieAPI.getMovies().then((res) => res.json()),
   });
   if (isPending) return "Loading...";
+  console.log(data);
 
   if (error) return "An error has occurred: " + error.message;
-  return <div>{JSON.stringify(data)}</div>;
+  return <div>Data</div>;
 }

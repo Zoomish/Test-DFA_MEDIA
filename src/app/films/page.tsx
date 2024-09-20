7ef02a24f7e636ea9d0e69e1c956c24d54a68422
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-20 19:58:06 +0300

    Remove "use client" directive and useRouter import, replace router.query.slug with "aads" in Home component.

--- src/app/films/page.tsx
+++ src/app/films/page.tsx
@@ -1,10 +1,7 @@
-"use client"
-import { useRouter } from "next/router";
 function Home() {
-  const router = useRouter();
   return (
     <div>
-      <p>Post: {router.query.slug}</p>
+        aads
     </div>
   );
 }

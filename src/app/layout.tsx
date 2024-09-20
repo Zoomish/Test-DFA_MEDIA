b6550e423f2e616c3b02873f909b295c8af8088c
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-20 20:45:01 +0300

    "Wrap layout with QueryClientProvider and create new QueryClient instance"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,24 +4,28 @@ import "./globals.css";
 import styles from "./layout.module.scss";
 import { Layout } from "antd";
 import FooterC from "@/components/Footer/FooterC";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const queryClient = new QueryClient();
   const { Header, Content, Footer } = Layout;
   return (
-    <html lang="en">
-      <body className={styles.body}>
-        <Header>
-          <HeaderC />
-        </Header>
-        <Content>{children}</Content>
-        <Footer>
-          <FooterC />
-        </Footer>
-      </body>
-    </html>
+    <QueryClientProvider client={queryClient}>
+      <html lang="en">
+        <body className={styles.body}>
+          <Header>
+            <HeaderC />
+          </Header>
+          <Content>{children}</Content>
+          <Footer>
+            <FooterC />
+          </Footer>
+        </body>
+      </html>
+    </QueryClientProvider>
   );
 }

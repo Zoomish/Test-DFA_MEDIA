e894476ca6cf476a2ea9c752a3fcb1c573ecbb2a
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-20 22:18:35 +0300

    "Wrapped footer text in a paragraph element"

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,10 @@ import styles from "./footer.module.scss";
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      Copyright &copy; {new Date().getFullYear()} Zoomish. All rights reserved.
+      <p>
+        Copyright &copy; {new Date().getFullYear()} Zoomish. All rights
+        reserved.
+      </p>
     </div>
   );
 }

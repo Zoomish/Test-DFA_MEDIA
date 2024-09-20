6446f8872e4f4ff87f7760e8a18a135249c17e8f
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-20 22:24:14 +0300

    "Wrap RootLayout with BrowserRouter and move QueryClientProvider inside it"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import styles from "./layout.module.scss";
 import Footer from "@/components/Footer/Footer";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
 
 export default function RootLayout({
   children,
@@ -12,18 +13,20 @@ export default function RootLayout({
 }>) {
   const queryClient = new QueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <html lang="en">
-        <body className={styles.body}>
-          <header>
-            <Header />
-          </header>
-          <main>{children}</main>
-          <footer>
-            <Footer />
-          </footer>
-        </body>
-      </html>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <html lang="en">
+          <body className={styles.body}>
+            <header>
+              <Header />
+            </header>
+            <main>{children}</main>
+            <footer>
+              <Footer />
+            </footer>
+          </body>
+        </html>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }

342e247943e6fb5e9de934bd235fb99adb9ac325
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-20 19:52:28 +0300

    "Add Home function component to films/page.tsx, using useRouter from next/router to display router.query.slug"

---
+++ src/app/films/page.tsx
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router";
+export default function Home() {
+  const router = useRouter();
+  return (
+    <div>
+      <p>Post: {router.query.slug}</p>
+    </div>
+  );
+}

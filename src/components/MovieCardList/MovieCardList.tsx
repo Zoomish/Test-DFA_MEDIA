28ae2253a0c60c2e886374ac8a4b1cf956e4e1ff
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 19:22:37 +0300

    "Updated MovieCardList component to use CSS module, renamed stylesheet to movieCardList.module.scss and added styles for .container class."

--- src/components/MovieCardList/MovieCardList.tsx
+++ src/components/MovieCardList/MovieCardList.tsx
@@ -1,5 +1,5 @@
 "use client";
-import styles from "./movieCardList.scss";
+import styles from "./movieCardList.module.scss";
 
 export default function Home() {
   return <div className={styles.container}></div>;

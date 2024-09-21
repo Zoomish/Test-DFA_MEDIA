d81e548ae5d0e07ff58d6879db2e6bbfd4389128
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 16:33:38 +0300

    "Updated package-lock.json and package.json to add @ant-design/icons dependency, removed react-router and react-router-dom, and updated sass version. Modified Header.tsx to change link URLs and add new links."

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,22 @@ import styles from "./header.module.scss";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <Link href={"/"} className={styles.a}>Zoomish</Link>
+      <Link href={"https://github.com/Zoomish"} className={styles.a}>
+        Zoomish
+      </Link>
       <div>
-        <a href="/sign-in">Sign In</a>
-        <a href="/sign-up">Sign Up</a>
+        <Link href={"/movie"} className={styles.a}>
+          Movies
+        </Link>
+        <Link href={"/about"} className={styles.a}>
+          About
+        </Link>
+        <Link href={"/contact"} className={styles.a}>
+          Contact
+        </Link>
+        <Link href={"/user/sign-up"} className={styles.a}>
+          Sign Up
+        </Link>
       </div>
     </div>
   );

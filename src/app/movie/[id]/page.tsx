41e2b17688ec94ef0b8b207b5ce166df934daddc
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 14:47:42 +0300

    "Added import for setAuthState, useAppDispatch, and a button to toggle authState"

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,13 +1,16 @@
 "use client";
-import { useAppSelector } from "@/redux/hooks";
+import { setAuthState } from "@/redux/authSlice/authSlice";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import { usePathname } from "next/navigation";
 export default function Home() {
   const router = usePathname().split("/");
+  const dispatch = useAppDispatch();
   const id = router[router.length - 1];
   const authState = useAppSelector((state) => state.auth.authState);
   return (
     <div>
       <p>Film: {authState}</p>
+      <button onClick={() => dispatch(setAuthState(!authState))}>Click</button>
     </div>
   );
 }

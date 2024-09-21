41a3fc34fdde81c347328f88fac69b71ed622a07
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 19:51:07 +0300

    "Extracted MovieCard component, moved styles to module.scss, and removed duplicate styles"

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,14 @@
 "use client";
+import { TMovieShort } from "@/utils/typesFromBackend";
 import styles from "./movieCard.module.scss";
+import { BASE_URL_IMG } from "@/utils/const";
 
-export default function MovieCard() {
-  return <div className={styles.container}></div>;
+export default function MovieCard(movie: TMovieShort) {
+  return (
+    <div key={movie.id} className={styles.movieCard}>
+      <img src={BASE_URL_IMG + movie.poster_path} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>{movie.overview}</p>
+    </div>
+  );
 }
--- src/components/MovieCardList/MovieCardList.tsx
+++ src/components/MovieCardList/MovieCardList.tsx
@@ -1,18 +1,15 @@
 "use client";
 import { useAppSelector } from "@/redux/hooks";
-import styles from "./movieCardList.scss";
-import { BASE_URL_IMG } from "@/utils/const";
+import MovieCard from "../MovieCard/MovieCard";
+import { TMovieShort } from "@/utils/typesFromBackend";
+import styles from "./movieCardList.module.scss";
 
 export default function MovieCardList() {
   const movies = useAppSelector((state) => state.movies.movies);
   return (
     <div className={styles.container}>
-      {movies.map((movie) => (
-        <div key={movie.id} className={styles.movieCard}>
-          <img src={BASE_URL_IMG + movie.poster_path} alt={movie.title} />
-          <h3>{movie.title}</h3>
-          <p>{movie.overview}</p>
-        </div>
+      {movies.map((movie: TMovieShort) => (
+        <MovieCard movie={movie} key={movie.id} />
       ))}
     </div>
   );

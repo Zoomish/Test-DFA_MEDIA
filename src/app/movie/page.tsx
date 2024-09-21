c38550affa06f5b81250227e4d163b05b234831d
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 18:15:38 +0300

    init

--- src/app/movie/page.tsx
+++ src/app/movie/page.tsx
@@ -4,6 +4,7 @@ import * as movieAPI from "@/utils/api/movie-api";
 import styles from "./movie.module.scss";
 import { Carousel } from "antd";
 import { BASE_URL_IMG } from "@/utils/const";
+import { TMovieShort } from "@/utils/typesFromBackend";
 
 export default function Home() {
   const { isPending, error, data } = useQuery({
@@ -23,7 +24,7 @@ export default function Home() {
         autoplaySpeed={6000}
         className={styles.carousel}
       >
-        {data.map((movie: any) => (
+        {data.map((movie: TMovieShort) => (
           <div className={styles.carouselel} key={movie.id}>
             <img
               src={BASE_URL_IMG + movie.backdrop_path}
@@ -35,6 +36,11 @@ export default function Home() {
               className={styles.img}
               alt={movie.title}
             />
+            <p className={styles.text_title}>{movie.title}</p>
+            <p className={styles.text_date}>
+              {movie.release_date} &nbsp;&nbsp;&nbsp;&nbsp;{Math.floor(movie.vote_average * 100) / 100}★
+            </p>
+            <p className={styles.text_overview}>{movie.overview}</p>
           </div>
         ))}
       </Carousel>

10c11906bc02eab249351b973e6dc5c221a5a2b4
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 15:33:00 +0300

    "Refactor Movie page component to remove Redux and API logic, replacing it with a Movie component import"

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,28 +1,8 @@
-"use client";
-import { useAppDispatch, useAppSelector } from "@/redux/hooks";
-import { useQuery } from "@tanstack/react-query";
-import { usePathname } from "next/navigation";
-import * as movieAPI from "@/utils/api/movie-api";
-import { setMovieState } from "@/redux/movieSlice/movieSlice";
-
-export default function Home() {
-  const router = usePathname().split("/");
-  const id = router[router.length - 1];
-  const dispatch = useAppDispatch();
-  const { isPending, error } = useQuery({
-    queryKey: ["movie"],
-    queryFn: () =>
-      movieAPI.getMovie(+id).then((res) => dispatch(setMovieState(res))),
-  });
-
-  const movie = useAppSelector((state) => state.movies.movie);
-
-  if (isPending) return "Loading...";
-
-  if (error) return "An error has occurred: " + error.message;
+import Movie from "@/components/Movie/Movie";
+export default function MovieContainer() {
   return (
     <div>
-      <p>Film: {JSON.stringify(movie)}</p>
+      <Movie />
     </div>
   );
 }
---
+++ src/components/Movie/Movie.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { useQuery } from "@tanstack/react-query";
+import { usePathname } from "next/navigation";
+import * as movieAPI from "@/utils/api/movie-api";
+import { setMovieState } from "@/redux/movieSlice/movieSlice";
+
+export default function Movie() {
+  const router = usePathname().split("/");
+  const id = router[router.length - 1];
+  const dispatch = useAppDispatch();
+  const { isPending, error } = useQuery({
+    queryKey: ["movie"],
+    queryFn: () =>
+      movieAPI.getMovie(+id).then((res) => dispatch(setMovieState(res))),
+  });
+
+  const movie = useAppSelector((state) => state.movies.movie);
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+  return (
+    <div>
+      <p>Film: {JSON.stringify(movie)}</p>
+    </div>
+  );
+}

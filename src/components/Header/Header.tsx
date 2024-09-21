2e38da6b9e96197e83a0a749e55c39955afed977
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 16:26:08 +0300

    iit

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import styles from "./header.module.scss";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <p>Header</p>
+      <Link href={"/"} className={styles.a}>Zoomish</Link>
       <div>
         <a href="/sign-in">Sign In</a>
         <a href="/sign-up">Sign Up</a>

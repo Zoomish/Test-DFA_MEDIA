051af434d05ebb3b6d10a79c6237ea1f245c30f4
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 16:42:31 +0300

    "Removed About and Contact links from Header, added links class to container, and updated styles"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,15 +6,9 @@ export default function Header() {
       <Link href={"https://github.com/Zoomish"} className={styles.a}>
         Zoomish
       </Link>
-      <div>
+      <div className={styles.links}>
         <Link href={"/movie"} className={styles.a}>
           Movies
-        </Link>
-        <Link href={"/about"} className={styles.a}>
-          About
-        </Link>
-        <Link href={"/contact"} className={styles.a}>
-          Contact
         </Link>
         <Link href={"/user/sign-up"} className={styles.a}>
           Sign Up

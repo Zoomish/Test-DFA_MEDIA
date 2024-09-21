c6f4a21f76a3164ed08e3c769c2342e73712e5ea
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 19:04:24 +0300

    "Added movies state and setMoviesState action to movieSlice"

--- src/redux/movieSlice/movieSlice.ts
+++ src/redux/movieSlice/movieSlice.ts
@@ -2,10 +2,12 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 export interface IMovieState {
   movie: object;
+  movies: object[];
 }
 
 const initialState: IMovieState = {
   movie: {},
+  movies: [],
 };
 
 export const movieSlice = createSlice({
@@ -15,8 +17,12 @@ export const movieSlice = createSlice({
     setMovieState: (state, action: PayloadAction<object>) => {
       state.movie = action.payload;
     },
+    setMoviesState: (state, action: PayloadAction<Array<object>>) => {
+      state.movies = action.payload;
+    },
   },
 });
 
 export const { setMovieState } = movieSlice.actions;
+export const { setMoviesState } = movieSlice.actions;
 export const movieReducer = movieSlice.reducer;

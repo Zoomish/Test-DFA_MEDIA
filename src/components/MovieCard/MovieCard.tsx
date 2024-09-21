ab9311fccb853bac204e21461a0ea7cca17a2231
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-21 20:39:48 +0300

    "Updated MovieCard component to use Antd Popover, changed width of .movieCard to 200px, and adjusted gap in movieCardList.module.scss"

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -2,13 +2,22 @@
 import { TMovieShort } from "@/utils/typesFromBackend";
 import styles from "./movieCard.module.scss";
 import { BASE_URL_IMG } from "@/utils/const";
+import { Popover } from "antd";
 
-export default function MovieCard({ movie }: { movie: TMovieShort}) {
-  return (
-    <div key={movie.id} className={styles.movieCard}>
-      <img src={BASE_URL_IMG + movie.poster_path} alt={movie.title} />
-      <h3>{movie.title}</h3>
-      <p>{movie.overview}</p>
+export default function MovieCard({ movie }: { movie: TMovieShort }) {
+  const content = (
+    <div>
+      <p>Content</p>
+      <p>Content</p>
     </div>
+  );
+  return (
+    <Popover placement="rightTop" content={content} title="Title">
+      <div key={movie.id} className={styles.movieCard}>
+        <img src={BASE_URL_IMG + movie.poster_path} alt={movie.title} />
+        <h3>{movie.title}</h3>
+        <p>{movie.overview}</p>
+      </div>
+    </Popover>
   );
 }

281edc8be75af8ae87b7147fdb0e622a0cc616a9
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-22 10:56:04 +0300

    Added conditional rendering of Carousel component based on movies length.

--- src/components/Clider/Clider.tsx
+++ src/components/Clider/Clider.tsx
@@ -9,38 +9,40 @@ import Link from "next/link";
 export default function Slider() {
   const movies = useAppSelector((state) => state.movies.movies);
   return (
-    <Carousel
-      autoplay
-      dotPosition="left"
-      dots={false}
-      infinite
-      autoplaySpeed={6000}
-      className={styles.carousel}
-    >
-      {movies.map((movie: TMovieShort) => (
-        <Link href={`/movie/${movie.id}`} key={movie.id}>
-          <div className={styles.carouselel} key={movie.id}>
-            <img
-              src={BASE_URL_IMG + movie.backdrop_path}
-              className={styles.imgBackground}
-              alt={movie.title}
-            />
-            <img
-              src={BASE_URL_IMG + movie.backdrop_path}
-              className={styles.img}
-              alt={movie.title}
-            />
-            <div className={styles.text}>
-              <p className={styles.text_title}>{movie.title}</p>
-              <p className={styles.text_date}>
-                {movie.release_date} &nbsp;&nbsp;&nbsp;&nbsp;
-                {Math.floor(movie.vote_average * 10) / 10}★
-              </p>
-              <p className={styles.text_overview}>{movie.overview}</p>
+    movies.length && (
+      <Carousel
+        autoplay
+        dotPosition="left"
+        dots={false}
+        infinite
+        autoplaySpeed={6000}
+        className={styles.carousel}
+      >
+        {movies.map((movie: TMovieShort) => (
+          <Link href={`/movie/${movie.id}`} key={movie.id}>
+            <div className={styles.carouselel} key={movie.id}>
+              <img
+                src={BASE_URL_IMG + movie.backdrop_path}
+                className={styles.imgBackground}
+                alt={movie.title}
+              />
+              <img
+                src={BASE_URL_IMG + movie.backdrop_path}
+                className={styles.img}
+                alt={movie.title}
+              />
+              <div className={styles.text}>
+                <p className={styles.text_title}>{movie.title}</p>
+                <p className={styles.text_date}>
+                  {movie.release_date} &nbsp;&nbsp;&nbsp;&nbsp;
+                  {Math.floor(movie.vote_average * 10) / 10}★
+                </p>
+                <p className={styles.text_overview}>{movie.overview}</p>
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
-    </Carousel>
+          </Link>
+        ))}
+      </Carousel>
+    )
   );
 }

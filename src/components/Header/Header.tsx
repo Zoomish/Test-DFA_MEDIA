ca0516d57fa1b3013f897ec72b636a32c7d2abd4
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-22 21:27:10 +0300

    "Added @uidotdev/usehooks dependency and implemented useDebounce hook in Header component; updated store reducer to include searchReducer"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,11 @@
+"use client";
 import Link from "next/link";
 import styles from "./header.module.scss";
+import { useState } from "react";
+import { useDebounce } from "@uidotdev/usehooks";
 export default function Header() {
+  const [filter, setFilter] = useState(undefined);
+  const debouncedFilter = useDebounce(filter, 500);
   return (
     <div className={styles.header}>
       <Link href={"https://github.com/Zoomish"} className={styles.a}>

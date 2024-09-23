10aa02c90c24bd7cf4767473368296043907ed29
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-23 22:04:49 +0300

    Added CSS imports and classes to Filter component, and updated padding in MovieCardList component's SCSS file.

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import styles from "./filter.module.scss";
 import { Popover } from "antd";
 
 export default function Filter() {
@@ -10,8 +11,8 @@ export default function Filter() {
         </div>
       );
   return (
-    <div>
-      <Popover content={content} title="Title" trigger="click">
+    <div className={styles.filter}>
+      <Popover content={content} title="Title" trigger="click" className={styles.button}>
         <p>Фильтры: </p>
       </Popover>
     </div>

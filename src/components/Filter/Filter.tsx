26c89729791476d1d60c27439645a4a5c67a881a
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-23 21:56:54 +0300

    init

---
+++ src/components/Filter/Filter.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Popover } from "antd";
+
+export default function Filter() {
+  return (
+    <div>
+      <Popover>
+        <p>Фильтры</p>
+      </Popover>
+    </div>
+  );
+}

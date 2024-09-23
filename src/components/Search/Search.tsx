90174fe791f1f8e24be96c6b1f78f4f94747a952
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-23 21:35:26 +0300

    init

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -4,7 +4,10 @@ import { useDebounce } from "@uidotdev/usehooks";
 import { useAppDispatch } from "@/redux/hooks";
 import { setSearchState } from "@/redux/seachSlice/searchSlice";
 import { Input } from "antd";
+import { usePathname, useRouter } from "next/navigation";
 export default function Search() {
+  const router = useRouter();
+  const location = usePathname().split("/");
   const [filter, setFilter] = useState("");
   const dispatch = useAppDispatch();
   const debouncedFilter = useDebounce(filter, 500);
@@ -12,6 +15,9 @@ export default function Search() {
     target: { value: SetStateAction<string> };
   }) => {
     setFilter(e.target.value);
+    if (!isNaN(Number(location[location.length - 1]))) {
+      router.push("/movie");
+    }
   };
   useEffect(() => {
     dispatch(setSearchState(debouncedFilter));

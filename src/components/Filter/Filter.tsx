51d0dd86c027efc0a3b199aef1254d40ffb4dd8f
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-23 21:59:16 +0300

    "Added content and title to Popover component in Filter.tsx"

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -3,9 +3,15 @@
 import { Popover } from "antd";
 
 export default function Filter() {
+    const content = (
+        <div>
+          <p>Content</p>
+          <p>Content</p>
+        </div>
+      );
   return (
     <div>
-      <Popover>
+      <Popover content={content} title="Title" trigger="click">
         <p>Фильтры: </p>
       </Popover>
     </div>

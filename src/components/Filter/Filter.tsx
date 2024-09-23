dc6e58f8e985782a87c03a19a6db66b88d70acb6
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-23 22:06:28 +0300

    "Updated Filter component to use Antd Form and Input, replaced static content with form items"

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,18 +1,30 @@
 "use client";
 
 import styles from "./filter.module.scss";
-import { Popover } from "antd";
+import { Form, Input, Popover } from "antd";
 
 export default function Filter() {
-    const content = (
-        <div>
-          <p>Content</p>
-          <p>Content</p>
-        </div>
-      );
+  const content = (
+    <Form>
+      <Form.Item label="Жанр">
+        <Input></Input>
+      </Form.Item>
+      <Form.Item label="Рейтинг">
+        <Input></Input>
+      </Form.Item>
+      <Form.Item label="Год выпуска">
+        <Input></Input>
+      </Form.Item>
+    </Form>
+  );
   return (
     <div className={styles.filter}>
-      <Popover content={content} title="Title" trigger="click" className={styles.button}>
+      <Popover
+        content={content}
+        title="Фильтры:"
+        trigger="click"
+        className={styles.button}
+      >
         <p>Фильтры: </p>
       </Popover>
     </div>

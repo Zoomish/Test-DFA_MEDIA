931851688209ede02ba6a9fd7d54b6515c0e5d50
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-24 14:47:18 +0300

    "Added form instance and submission handling to Filter component"

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,19 +1,29 @@
 "use client";
 
 import styles from "./filter.module.scss";
-import { Form, Input, Popover } from "antd";
+import { Button, Form, Input, Popover } from "antd";
 
 export default function Filter() {
+  const [form] = Form.useForm();
+  form.setFieldValue("release_year", new Date().getFullYear());
+  const onFinish = (values: any): void => {
+    console.log(values);
+  };
   const content = (
-    <Form>
-      <Form.Item label="Жанр">
+    <Form form={form} onFinish={onFinish}>
+      <Form.Item label="Жанр" name="genre">
         <Input></Input>
       </Form.Item>
-      <Form.Item label="Рейтинг">
+      <Form.Item label="Рейтинг" name="vote_average">
         <Input></Input>
       </Form.Item>
-      <Form.Item label="Год выпуска">
+      <Form.Item label="Год выпуска" name="release_year">
         <Input></Input>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Подтвердить
+        </Button>
       </Form.Item>
     </Form>
   );

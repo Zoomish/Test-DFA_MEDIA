418ff903547fe19c3ae342f2b404e6219b01ff95
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-24 15:01:15 +0300

    "Updated Filter component to import and use filterSlice actions, and modified onFinish function to dispatch these actions with form values."

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -4,6 +4,7 @@ import { useEffect } from "react";
 import styles from "./filter.module.scss";
 import { Button, Form, Input, Popover } from "antd";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { IFilterState, setGenreState, setRatingState, setSortState, setYearState } from "@/redux/filterSlice/filterSlice";
 
 export default function Filter() {
   const [form] = Form.useForm();
@@ -14,8 +15,11 @@ export default function Filter() {
     form.setFieldValue("genre", filters.genre);
     form.setFieldValue("vote_average", filters.vote_average);
   }, []);
-  const onFinish = (values: any): void => {
-    console.log(values);
+  const onFinish = (values: IFilterState): void => {
+    dispatch(setYearState(values.release_year));
+    dispatch(setGenreState(values.genre));
+    dispatch(setSortState(values.sort_by));
+    dispatch(setRatingState(values.vote_average));
   };
   const content = (
     <Form form={form} onFinish={onFinish}>

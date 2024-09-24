9ff49284033fb884c8b8b0518834d870e1d52b1e
Egor Rezvyi <99123419+Zoomish@users.noreply.github.com>
2024-09-24 14:59:03 +0300

    "Added React hooks and Redux integration to Filter component"

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { useEffect } from "react";
 import styles from "./filter.module.scss";
 import { Button, Form, Input, Popover } from "antd";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 
 export default function Filter() {
   const [form] = Form.useForm();
-  form.setFieldValue("release_year", new Date().getFullYear());
+  const filters = useAppSelector((state) => state.filter);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    form.setFieldValue("release_year", filters.release_year);
+    form.setFieldValue("genre", filters.genre);
+    form.setFieldValue("vote_average", filters.vote_average);
+  }, []);
   const onFinish = (values: any): void => {
     console.log(values);
   };
